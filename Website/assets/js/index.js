4bb968c54fb3bfae352b59f443e7955816820ca3
Denizm4 <140453516+Denizm4@users.noreply.github.com>
2024-04-17 09:56:58 +0300

    Add files via upload

---
+++ Website/assets/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    ma5menu({
+        menu: '.mobile-menu',
+        activeClass: 'active',
+        footer: '#ma5menu-tools',
+        position: 'left',
+        closeOnBodyClick: true
+    });
+});
